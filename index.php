a6b3fa360614eacdd431383a6c80281e9bcef3c4
Sergio Daniel Silva Saldarriaga <Ssilva@intergrupo.com>
2016-12-21 10:36:19 -0500

    agregar logout e imprimir el token despues del login

--- index.php
+++ index.php
@@ -7,12 +7,14 @@
 <body>
 	<div id='dz-root'></div>
 	<input type="button" onclick="DZ.player.playAlbum(6125830);" value="Play"/>
-	<input type="button" onclick="DZ.login()" value="Login"/>
+	<input type="button" onclick="login()" value="Login1"/>
+	<input type="button" onclick="DZ.login()" value="Login2"/>
+	<input type="button" onclick="DZ.logout()" value="logout"/>
 	<script>
 
 		DZ.init({
 			appId  : '218204',
-			channelUrl : 'https://immense-woodland-64988.herokuapp.com/',
+			channelUrl : 'http://immense-woodland-64988.herokuapp.com/',
 			player: {
 				onload: function (response) {
 					console.log('estamos ready', response);
@@ -23,6 +25,13 @@
 		DZ.api('/user/me', function(response){
 			console.log("My name", response);
 		});
+
+		function login(){
+			DZ.login(function(response){
+				console.log("el response del login");
+				console.log(response);
+			});
+		}
 	</script>
 </body>
 </html>
